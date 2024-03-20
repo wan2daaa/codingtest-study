99bcfd648e9965c87f626c70b39586ab2ff7bea8
wan2daaa <wan2daaa@gmail.com>
2024-03-20 16:32:57 +0900

    큐 공부

--- src/Test.java
+++ src/Test.java
@@ -4,6 +4,10 @@ import java.util.List;
 public class Test {
 
   public static void main(String[] args) {
+
+    /**
+     * 배열
+     */
     String[] testList = new String[5];
 
     testList[0] = "1";
@@ -26,6 +30,43 @@ public class Test {
 
     System.out.println(stringList.get(0).indexOf("0"));
 
+    /**
+     * 큐
+     */
+
+    class QueueImpl<T>{
+      private ArrayList<T> queue = new ArrayList<T>();
+
+      public void enqueue(T item) {
+        queue.add(item);
+      }
+
+      public T dequeue() {
+        if (queue.isEmpty()) {
+          return null;
+        }
+
+//        T temp = queue.get(0);
+//        queue.remove(0);
+//        return temp;
+
+        return queue.remove(0);
+      }
+
+      public boolean isEmpty() {
+        return queue.isEmpty();
+      }
+
+    }
+
+    QueueImpl<Integer> queue = new QueueImpl<>();
+    queue.enqueue(1);
+    queue.enqueue(2);
+    queue.enqueue(3);
+
+    System.out.println("queue remove fir"+queue.dequeue());
+    System.out.println("queue remove sec"+queue.dequeue());
+    System.out.println("queue remove third"+queue.dequeue());
 
   }
 
