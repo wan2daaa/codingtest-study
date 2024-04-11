9ef741b6820e75a921e7d180e9309a85e04834c0
wan2daaa <wan2daaa@gmail.com>
2024-04-11 18:36:34 +0900

    최소 신장 트리 (크루스칼 알고리즘) 공부
    최소 신장 트리는 기존 그래프의 모든 노드를 포함하면서, 모든 노드가 서로 연결 되지만, 사이클이 생기면 안됨!
    
    이 중, 간선의 가중치 합이 최소인 것을 최소 신장 트리 라고 한다!
    
    최소 신장 트리 알고리즘에는 대표적으로 크루스칼 알고리즘과 프림 알고리즘이 있는데,
    크루스칼 알고리즘 부터 공부
    
    - 탐욕 알고리즘을 기초로 하고 있고,
    모든 정점을 독립적인 집합으로 만들고,
    모든 간선을 비용을 기준으로 정렬하고, 비용이 작은 간선부터 양 끝의 두 정점을 비교하여
    두 정점의 취상위 정점을 확인하고, 서로 다를 경우 두 정점을 연결하는 방법!
    
    3 번과정에서 시간 복잡도를 줄이기 위해 Union-Find 알고리즘과 path compression 기법을 활용하여 시간 복잡도를 O(1) 상수에 가깝게 만들었다!
    
    결과적으로 시간 복잡도는 ElogE 정렬이 제일 오래 걸린다.

---
+++ src/알고리즘/최소신장트리/Kruskal.java
@@ -0,0 +1,130 @@
+package 알고리즘.최소신장트리;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+
+class Kruskal {
+
+  HashMap<String, String> parent = new HashMap<>();
+  HashMap<String, Integer> rank = new HashMap<>();
+
+  private String find(String node) {
+    // path compression 기법
+    if (parent.get(node) != node) {
+      parent.put(node, find(parent.get(node)));
+    }
+    return parent.get(node);
+  }
+
+  //사이클이 안 생긴다고 가정
+  private void union(String nodeV, String nodeU) {
+    String root1 = find(nodeV);
+    String root2 = find(nodeU);
+
+    //Union-by-rank
+    if (rank.get(root1) > rank.get(root2)) {
+      parent.put(root2, root1);
+    } else {
+      parent.put(root1, root2);
+      if (rank.get(root1) == rank.get(root2)) {
+        rank.put(root2, rank.get(root2) + 1);
+      }
+    }
+  }
+
+
+  private void makeSet(String node) {
+    this.parent.put(node, node);
+    this.rank.put(node, 0);
+  }
+
+  public ArrayList<Edge> kruskalFunc(ArrayList<String> vetices, ArrayList<Edge> edges) {
+    ArrayList<Edge> mst = new ArrayList<>();
+    Edge currentEdge;
+
+    //Initialization
+    for (String vetice : vetices) {
+      makeSet(vetice);
+    }
+
+    // 간선 weight 기반, sorting
+    Collections.sort(edges);
+
+    for (int index = 0; index < edges.size(); index++) {
+      currentEdge = edges.get(index);
+      if (find(currentEdge.nodeV) != find(currentEdge.nodeU)) {
+        union(currentEdge.nodeV, currentEdge.nodeU);
+        mst.add(currentEdge);
+      }
+    }
+
+    return mst;
+  }
+
+  public static void main(String[] args) {
+    ArrayList<String> vertices = new ArrayList<String>(
+        Arrays.asList("A", "B", "C", "D", "E", "F", "G"));
+
+    ArrayList<Edge> edges = new ArrayList<>();
+    addEdgeDatas(edges);
+
+    Kruskal kruskal = new Kruskal();
+    ArrayList<Edge> kruskalFunc = kruskal.kruskalFunc(vertices, edges);
+    System.out.println("kruskalFunc = " + kruskalFunc);
+
+  }
+
+  private static class Edge implements Comparable<Edge> {
+
+    public int weight;
+    public String nodeV;
+    public String nodeU;
+
+    public Edge(int weight, String nodeV, String nodeU) {
+      this.weight = weight;
+      this.nodeV = nodeV;
+      this.nodeU = nodeU;
+    }
+
+    @Override
+    public String toString() {
+      return
+         "\n" +"weight=" + weight +
+          ", nodeV='" + nodeV + '\'' +
+          ", nodeU='" + nodeU + '\'' ;
+    }
+
+    @Override
+    public int compareTo(Edge edge) {
+      return this.weight - edge.weight;
+    }
+
+  }
+
+  private static void addEdgeDatas(ArrayList<Edge> edges) {
+    edges.add(new Edge(7, "A", "B"));
+    edges.add(new Edge(5, "A", "D"));
+    edges.add(new Edge(7, "B", "A"));
+    edges.add(new Edge(8, "B", "C"));
+    edges.add(new Edge(9, "B", "D"));
+    edges.add(new Edge(7, "B", "E"));
+    edges.add(new Edge(8, "C", "B"));
+    edges.add(new Edge(5, "C", "E"));
+    edges.add(new Edge(5, "D", "A"));
+    edges.add(new Edge(9, "D", "B"));
+    edges.add(new Edge(7, "D", "E"));
+    edges.add(new Edge(6, "D", "F"));
+    edges.add(new Edge(7, "E", "B"));
+    edges.add(new Edge(5, "E", "C"));
+    edges.add(new Edge(7, "E", "D"));
+    edges.add(new Edge(8, "E", "F"));
+    edges.add(new Edge(9, "E", "G"));
+    edges.add(new Edge(6, "F", "D"));
+    edges.add(new Edge(8, "F", "E"));
+    edges.add(new Edge(11, "F", "G"));
+    edges.add(new Edge(9, "G", "E"));
+    edges.add(new Edge(11, "G", "F"));
+  }
+}
